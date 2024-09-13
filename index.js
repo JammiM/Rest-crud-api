b50f2e337a6ab43d3b67964f401bda352d9ded09
James Morrissey <jammim19@gmail.com>
2024-09-13 12:05:13 +0100

    Set up express

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port : ${PORT}`);
+});
+
+app.get("/", (request, response) => {
+  response.send("API is ready");
+});
