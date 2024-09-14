4a9179cd87bdcc62a233ec4c99232b418aed37f2
James Morrissey <jammim19@gmail.com>
2024-09-14 10:13:53 +0100

    Incorperated mongoose and set up a connection to mongodb

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const app = express();
+const mongoose = require("mongoose");
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port : ${process.env.PORT}`);
@@ -9,3 +10,13 @@ app.listen(process.env.PORT, () => {
 app.get("/", (request, response) => {
   response.send("API is ready");
 });
+
+const db_username = encodeURIComponent(process.env.MONGODB_USERNAME);
+const db_password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
+const mongodb_connection_string = `mongodb+srv://${db_username}:${db_password}@backenddb.hy0xb.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB`;
+
+mongoose
+  .connect(mongodb_connection_string)
+  .then(() => console.log("Connected to DB!"))
+  .catch((err) => console.warn("Error connecting to DB : " + err));
